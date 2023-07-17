e92ca20e5c4e8929ebc936a14bb94080b52618e7
zena24-meet <siatbr.siatbr@gmail.com>
2023-07-17 14:13:00 +0300

    Update main.js

---
+++ main.js
@@ -0,0 +1,10 @@
+function changeBackgroundColor(color) 
+{
+  document.body.style.backgroundColor = color;
+}<button onclick="changeBackgroundColor('red')">Change BG Color to Red</button>
+<button onclick="changeBackgroundColor('#00FF00')">Change BG Color to Green</button>
+<button onclick="changeBackgroundColor('rgb(255, 0, 255)')">Change BG Color to Magenta</button>
+
+<button onclick="changeBackgroundColor('red')">Change BG Color to Red</button>
+<button onclick="changeBackgroundColor('#00FF00')">Change BG Color to Green</button>
+<button onclick="changeBackgroundColor('rgb(255, 0, 255)')">Change BG Color to Magenta</button>
